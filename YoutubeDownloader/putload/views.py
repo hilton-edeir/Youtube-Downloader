6e5be32ee85e3a8c117fec8f3ef2bab53e519fe2
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-16 00:40:59 -0100

    update

--- YoutubeDownloader/putload/views.py
+++ YoutubeDownloader/putload/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from pytube import YouTube
 
@@ -9,11 +10,13 @@ def home(request):
 def download(request):
     if request.method == "POST":
         link = request.POST['link']
-        yt = YouTube(link)
-        videos = yt.streams.all()
-        video = list(enumerate(videos))
 
-        for i in video:
-            print(i)
+        try:
+            yt = YouTube(link)
+            videos = yt.streams
+            video = list(enumerate(videos))
+
+        except:
+            messages.add_message(request, messages.ERROR, 'Download failed, please try again')
 
     return render(request, "home.html")
