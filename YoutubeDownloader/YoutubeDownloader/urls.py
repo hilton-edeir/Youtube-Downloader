e591debf7a4318a69191122f7d45aa41bf7d9227
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-16 02:19:09 -0100

    update

--- YoutubeDownloader/YoutubeDownloader/urls.py
+++ YoutubeDownloader/YoutubeDownloader/urls.py
@@ -20,5 +20,4 @@ from putload import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('<str:video>', views.download, name="download"),
 ]
--- YoutubeDownloader/putload/views.py
+++ YoutubeDownloader/putload/views.py
@@ -5,30 +5,24 @@ from pytube import YouTube
 
 def home(request):
     if request.method == "POST":
-        link = request.POST['link']
-
-        try:
-            yt = YouTube(link)
-            videos = yt.streams
-            list_videos = list(enumerate(videos))
-            empty = False
-
-            if len(list_videos) == 0:
-                empty = True
-
-            return render(request, "home.html", {"list_videos": list_videos, "empty": empty})
-
-        except:
-            messages.add_message(request, messages.ERROR, "We couldn't find nothing, please try again")
-
-    return render(request, "home.html")
+        link = request.POST['link_yt']
+        video = YouTube(link)
+        video.streams.get_by_itag(137)
 
 
-def download(request, video):
-    try:
-        video.download()
+        '''
+        list_videos = list(enumerate(videos))
+        empty = False
 
+        if len(list_videos) == 0:
+            empty = True
+
+        return render(request, "home.html", {"list_videos": list_videos, "empty": empty})
+       
     except:
-        messages.add_message(request, messages.ERROR, "Download failed, please try again")
+        messages.add_message(request, messages.ERROR, "We couldn't find nothing, please try again")
+         '''
 
     return render(request, "home.html")
+
+
