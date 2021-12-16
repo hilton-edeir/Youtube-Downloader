dfa7969f8074efe7e8b8bfd2115c10d611b382fa
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-16 00:05:03 -0100

    update

--- YoutubeDownloader/YoutubeDownloader/urls.py
+++ YoutubeDownloader/YoutubeDownloader/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('home/', views.home, name="home"),
+    path('download/<str:pk_course>', views.download, name="download"),
 ]
--- YoutubeDownloader/putload/views.py
+++ YoutubeDownloader/putload/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from pytube import YouTube
 
 
-def home(request, link):
-
+def home(request):
     return render(request, "home.html")
 
 
-def download():
-    pass
+def download(request, link):
+    yt = YouTube(link)
+    videos = yt.streams.all()
+    video = list(enumerate(videos))
+    return render(request, "home.html", {"video_list": video})
