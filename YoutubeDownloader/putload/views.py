40c58b1aa2d99eb74a5de0d97a9adf3cbb78dae2
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-17 10:09:16 -0100

    update

--- YoutubeDownloader/putload/views.py
+++ YoutubeDownloader/putload/views.py
@@ -2,27 +2,19 @@ from django.contrib import messages
 from django.shortcuts import render
 from pytube import YouTube
 
+PATH = "C:\users\edeir\Downloads"
+
 
 def home(request):
     if request.method == "POST":
-        link = request.POST['link_yt']
-        video = YouTube(link)
-        print(video.streams.filter(file_extension="mp4"))
-        video.streams.get_by_itag(18).download()
 
+        try:
+            link = request.POST['yt_link']
+            video = YouTube(link)
+            video.streams.get_by_itag(18).download(PATH)
 
-        '''
-        list_videos = list(enumerate(videos))
-        empty = False
-
-        if len(list_videos) == 0:
-            empty = True
-
-        return render(request, "home.html", {"list_videos": list_videos, "empty": empty})
-       
-    except:
-        messages.add_message(request, messages.ERROR, "We couldn't find nothing, please try again")
-         '''
+        except:
+            messages.add_message(request, messages.ERROR, "Something went wrong, please try again")
 
     return render(request, "home.html")
 
