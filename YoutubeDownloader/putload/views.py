196e33ece486ec014e20661aa3218b5abafb2540
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-17 13:20:30 -0100

    update

--- YoutubeDownloader/putload/views.py
+++ YoutubeDownloader/putload/views.py
@@ -9,7 +9,7 @@ def home(request):
             link = request.POST['yt_link']
             video = YouTube(link)
 
-            return render(request, "video_thumbnail.html", {"video": video, "link": link})
+            return render(request, "video.html", {"video": video, "link": link})
 
         except:
             messages.add_message(request, messages.ERROR, "Something went wrong, please try again")
